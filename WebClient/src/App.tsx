87d1303547a054858e01b9f103d8f038ba7bd8b4
JohnnyBoy2001 <JohnnyBoy2001@github.com>
2023-05-09 20:50:39 -0400

    Added pages and routes

--- WebClient/src/App.tsx
+++ WebClient/src/App.tsx
@@ -4,29 +4,22 @@ import { Routes, Route } from "@solidjs/router";
 import Home from "./Pages/Home";
 import logo from './logo.svg';
 import styles from './App.module.css';
+import Login from './Pages/Login';
 
 const App: Component = () => {
 
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> <br/>and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Routes>
+        <Routes>
             <Route path="/home" component={Home} />
-          </Routes>
-          Learn Solid
-        </a>
-        <Test/>
-      </header>
+            <Route path="/" component={Home} />
+            <Route path="/login" component={Login}/>
+            <Route path="/post_login_home" component={Login}/>
+            <Route path="/register" component={Login}/>
+            <Route path="/exercise_scheduler" component={Login}/>
+            <Route path="/weekly_workouts" component={Login}/>
+            <Route path="/exercise_summary" component={Login}/>
+        </Routes>
     </div>
   );
 };
