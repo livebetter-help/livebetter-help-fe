efe5f9741e29f4a32f45adad50dda599bc1ab79b
JohnnyBoy2001 <JohnnyBoy2001@github.com>
2023-05-09 23:11:28 -0400

    Conditionally Render Page On Button Click

--- WebClient/src/App.tsx
+++ WebClient/src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, JSX, Show, createSignal } from 'solid-js';
+import { Component, JSX, Show, createEffect, createSignal } from 'solid-js';
 import {Test} from "./components/Test";
 import { Routes, Route } from "@solidjs/router";
 import Home from "./Pages/Home";
@@ -13,33 +13,37 @@ import ExerciseSummary from './Pages/ExerciseSummary';
 
 const App: Component = () => {
   const [isLoggedIn, setIsLoggedIn] = createSignal(true);
+  const [isLoggedInText, setIsLoggedInText] = createSignal("In");
   const toggleLogin = () => setIsLoggedIn(!isLoggedIn());
+  createEffect(() => {
+    if(isLoggedIn()){
+      setIsLoggedInText("Out");
+    }else{
+      setIsLoggedInText("In");
+    }
+  })
   return (
     <div class={styles.App}>
+        <button onClick={() => toggleLogin()}>Log {isLoggedInText()}</button>
+        {/* <button onClick={() => toggleLogin()}>Log In</button> */}
         <Routes>
           <Show when={isLoggedIn()} fallback={
             <>
+            <button onClick={() => toggleLogin()}>Log Out</button>
               <Route path="/post_login_home" component={PostLoginHome}/>
               <Route path="/exercise_scheduler" component={ExerciseScheduler}/>
               <Route path="/weekly_workouts" component={WeeklyWorkouts}/>
               <Route path="/exercise_summary" component={ExerciseSummary}/>
+              
             </>
           }>
+            <button onClick={() => toggleLogin()}>Log In</button>
             <Route path="/home" component={Home} />
             <Route path="/" component={Home} />
             <Route path="/login" component={Login}/>
             <Route path="/register" component={RegisterPage}/>
           </Show>
-
-            
-            {/* <Route path="/post_login_home" component={Login}/>
-            
-            <Route path="/exercise_scheduler" component={Login}/>
-            <Route path="/weekly_workouts" component={Login}/>
-            <Route path="/exercise_summary" component={Login}/> */}
         </Routes>          
-        <button onClick={() => toggleLogin()}>Log In</button>
-        <p> This Button Will Allow You To Access The Loggin Restricted Pages </p>
     </div>
   );
 };
