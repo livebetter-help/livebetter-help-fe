3af40adaf7db10356df552363e390d2538085028
JohnnyBoy2001 <JohnnyBoy2001@github.com>
2023-05-09 21:47:07 -0400

    Added Button To Block Routes If Not Logged IN

--- WebClient/src/App.tsx
+++ WebClient/src/App.tsx
@@ -1,25 +1,45 @@
-import type { Component, JSX } from 'solid-js';
+import { Component, JSX, Show, createSignal } from 'solid-js';
 import {Test} from "./components/Test";
 import { Routes, Route } from "@solidjs/router";
 import Home from "./Pages/Home";
 import logo from './logo.svg';
 import styles from './App.module.css';
 import Login from './Pages/Login';
+import PostLoginHome from './Pages/PostLoginHome';
+import RegisterPage from './Pages/RegisterPage';
+import ExerciseScheduler from './Pages/ExerciseScheduler';
+import WeeklyWorkouts from './Pages/WeeklyWorkouts';
+import ExerciseSummary from './Pages/ExerciseSummary';
 
 const App: Component = () => {
-
+  const [isLoggedIn, setIsLoggedIn] = createSignal(true);
+  const toggleLogin = () => setIsLoggedIn(!isLoggedIn());
   return (
     <div class={styles.App}>
         <Routes>
+          <Show when={isLoggedIn()} fallback={
+            <>
+              <Route path="/post_login_home" component={PostLoginHome}/>
+              <Route path="/exercise_scheduler" component={ExerciseScheduler}/>
+              <Route path="/weekly_workouts" component={WeeklyWorkouts}/>
+              <Route path="/exercise_summary" component={ExerciseSummary}/>
+            </>
+          }>
             <Route path="/home" component={Home} />
             <Route path="/" component={Home} />
             <Route path="/login" component={Login}/>
-            <Route path="/post_login_home" component={Login}/>
-            <Route path="/register" component={Login}/>
+            <Route path="/register" component={RegisterPage}/>
+          </Show>
+
+            
+            {/* <Route path="/post_login_home" component={Login}/>
+            
             <Route path="/exercise_scheduler" component={Login}/>
             <Route path="/weekly_workouts" component={Login}/>
-            <Route path="/exercise_summary" component={Login}/>
-        </Routes>
+            <Route path="/exercise_summary" component={Login}/> */}
+        </Routes>          
+        <button onClick={() => toggleLogin()}>Log In</button>
+        <p> This Button Will Allow You To Access The Loggin Restricted Pages </p>
     </div>
   );
 };
